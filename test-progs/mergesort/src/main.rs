92b9c4ab00b2e1eef1cfa78e74262441492fdb5b
Nicholas Sim <nicholassimws@gmail.com>
2019-01-15 12:04:41 +0000

    prog: mergesort: initial setup

---
+++ test-progs/mergesort/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+    let mut arr = [73, 21, 2, 8, 1, 3];
+    arr.sort();
+    println!("{:?}", arr);
+    let mut vec = vec![53, 235, 45, 2, 65, -1];
+    vec.sort();
+    println!("{:?}", vec);
+    println!("{}", 5f32.sqrt());
+}
