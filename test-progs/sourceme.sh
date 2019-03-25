605e8b5cd4e4829df7109d3fd6c22ce003b84e09
Nicholas Sim <nicholassimws@gmail.com>
2019-03-25 18:20:21 +0000

    sourceme: env vars with new rust, llvm builds

---
+++ test-progs/sourceme.sh
@@ -0,0 +1,5 @@
+export LD_LIBRARY_PATH=$HOME/rust/build/x86_64-unknown-freebsd/stage1/lib:$LD_LIBRARY_PATH
+export RUSTC=$HOME/rust/build/x86_64-unknown-freebsd/stage1/bin/rustc
+export XARGO_RUST_SRC=$HOME/rust/src
+export PATH=$HOME/work/llvm-project/build/bin:$HOME/.cargo/bin:$HOME/rust/build/x86_64-unknown-freebsd/stage1/bin:$PATH
+export RUST_TARGET_PATH=/home/nwss2/m-proj/test-progs
