3919603c45b331eff7c015f880b19915ff62e01a
Nicholas Sim <nicholassimws@gmail.com>
2019-03-26 18:45:54 +0000

    prog: noc-ctr: add
    This example fails with cannot select: LDR when optimisations are off.
    Presumably this represents the unwrap action, which is failing to be
    translated. Hoping for a backend fix.

---
+++ test-progs/noc-ctr/src/main.rs
@@ -0,0 +1,58 @@
+#![feature(no_core)]
+#![feature(lang_items)]
+#![feature(optin_builtin_traits)]
+#![feature(on_unimplemented)]
+
+#![no_core]
+
+extern crate libcore_cheri as core;
+
+use core::option::Option;
+use core::prelude::*;
+
+#[cfg(target_os = "freebsd")]
+#[link(name = "c")]
+extern {
+    pub fn printf(format: *const u8, ...) -> i32;
+}
+
+#[lang = "start"]
+fn start(_main: *const u8, _argc: isize, _argv: *const *const u8) -> isize {
+    main();
+    0
+}
+
+fn print_sp(i: i32) {
+    unsafe {
+        printf(b"%d \0" as *const u8, i);
+    }
+}
+
+struct Counter {
+    count: usize,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter {count: 0}
+    }
+}
+
+impl Iterator for Counter {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<usize> {
+        self.count += 1;
+
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+fn main() {
+    let mut ctr = Counter::new();
+    print_sp(ctr.next().unwrap() as i32);
+}
