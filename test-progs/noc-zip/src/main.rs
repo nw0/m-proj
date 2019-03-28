829b660987b779a4c5a03235d79928922019c3dc
Nicholas Sim <nicholassimws@gmail.com>
2019-03-28 12:57:05 +0000

    prog: noc-zip: add some substance to prog

--- test-progs/noc-zip/src/main.rs
+++ test-progs/noc-zip/src/main.rs
@@ -21,8 +21,15 @@ fn start(_main: *const u8, _argc: isize, _argv: *const *const u8) -> isize {
     0
 }
 
+fn print_sp(i: i32) {
+    unsafe {
+        printf(b"%d \0" as *const u8, i);
+    }
+}
+
 fn main() {
-    let a1: [i32; 1] = [0];
-    let a2: [i32; 1] = [0];
-    let a3 = a1.iter().zip(a2.iter());
+    let a1: [i32; 4] = [1, 3, 4, 5];
+    let a2: [i32; 4] = [3, 3, 3, 4];
+    a1.iter().zip(a2.iter()).for_each(|(a, b)| print_sp(*a + *b));
+    unsafe { printf(b"\n\0" as *const u8); }
 }
