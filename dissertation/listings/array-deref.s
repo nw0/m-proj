c2beed8fdb86c34338c3004d5f8a32f734d24e2c
Nicholas Sim <nsim@posteo.net>
2019-04-26 14:24:52 +0100

    dis: eval: bounds check listing: update caption, comments

--- dissertation/listings/array-deref.s
+++ dissertation/listings/array-deref.s
@@ -3,8 +3,8 @@ idx:
     daddu   $3, $1, $25
     sltu    $1, $4, $6      ; idx < arr.len()
     beqz    $1, .LBB3_2
-    move    $2, $4
-    dsll    $1, $2, 3       ; offset = idx * 8
+    move    $2, $4          ; delay slot
+    dsll    $1, $2, 3       ; offset = idx * 8 (i64 array)
     daddu   $1, $5, $1      ; ptr = &arr + offset
     ld      $2, 0($1)       ; retval = *ptr
     jr      $ra
@@ -16,7 +16,7 @@ idx:
     daddiu  $gp, $3, %lo(%neg(%gp_rel(idx)))
     ld      $1, %got_page(.L__unnamed_2)($gp)
     daddiu  $4, $1, %got_ofst(.L__unnamed_2)
-    ld      $25, %call16(_ZN4core9panicking18panic_bounds_check17h34b80e64d41db052E)($gp)
+    ld      $25, %call16(core::panicking::panic_bounds_check)($gp)
     jalr    $25
     move    $5, $2
     break
