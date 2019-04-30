584544ea58bc0b1d926d0e4963a93a9967e82843
Nicholas Sim <nsim@posteo.net>
2019-04-30 16:17:53 +0100

    dis: rust: intrinsic indexing: clarify situations

---
+++ dissertation/listings/vec-index-trait.rs
@@ -0,0 +1,8 @@
+impl<T, I: SliceIndex<[T]>> Index<I> for Vec<T> {
+    type Output = I::Output;
+
+    #[inline]
+    fn index(&self, index: I) -> &Self::Output {
+        Index::index(&**self, index)
+    }
+}
