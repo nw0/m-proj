b1541ed491d810832bb339fe27c992fee521e87f
Nicholas Sim <nsim@posteo.net>
2019-05-03 19:43:08 +0100

    dis: eval: micro-repeat: listing

--- dissertation/listings/micro-repeat.rs
+++ dissertation/listings/micro-repeat.rs
@@ -1,5 +1,33 @@
 fn main() {
-    let s: [i64; 3] = [123, 456, 789];
-    let repetitions = 6148914691236517207;      // (2 ** 64 + 5) / 3
-    let mut buf = Vec::with_capacity(s.len() * rep);
+    use core::ptr::copy_nonoverlapping;
+
+    let s: [i64; 3] = [1, 2 ,3];
+    let reps = 6148914691236517207;             // (2 ** 64 + 5) / 3
+
+    let mut buf = Vec::with_capacity((s.len() * reps) as u64 as usize);
+    prntf!("capacity: %d\n", buf.capacity() as u64);    // 5
+    buf.extend(&s);
+
+    let mut v: Vec<i64> = Vec::new();
+    v.extend(&s);
+    v[0] = -1;
+    v.push(-4);
+    for x in &v { prntf!("%d ", *x); } prntf!("\n");    // -1 2 3 -4
+
+    {
+        let mut m = 8;      // 8 <= n >> 1; copy enough times to reach `v'
+        while m > 0 {
+            unsafe {
+                copy_nonoverlapping(            // generates memcpy
+                    buf.as_ptr(),               // out of bounds on 1st iter
+                    (buf.as_mut_ptr() as *mut i64).add(buf.len()),
+                    buf.len()
+                );                              // CHERI: length violation
+                let buf_len = buf.len();
+                buf.set_len(buf_len * 2);
+            }
+            m >>= 1;
+        }
+    }
+    for x in &v { prntf!("%d ", *x); } prntf!("\n");    // 1 2 3 1
 }
