efc152b246c9966407318fc88b191561f09b7b92
Nicholas Sim <nsim@posteo.net>
2019-05-03 15:03:08 +0100

    dis: eval: micro-cap: make prints rustlike

--- dissertation/listings/micro-capacity.rs
+++ dissertation/listings/micro-capacity.rs
@@ -3,24 +3,13 @@ fn main() {
 
     let mut deque = VecDeque::with_capacity(31);
     deque.push_front(5);
+    println!("{:?}", deque);                    // `[5]'
+    println!("{}, {}", deque.head, deque.tail); // `0, 31'
 
-    for x in &deque {
-        print_sp(*x);                           // `5'
-    }
-    unsafe { printf(b"\n\0" as *const u8); }
-    print_sp(deque.head as i64);                // `0'
-    print_sp(deque.tail as i64);                // `31'
-    unsafe { printf(b"\n\0" as *const u8); }
     deque.reserve(30);
-    print_sp(deque.head as i64);                // `32'
-    print_sp(deque.tail as i64);                // `31'
-    unsafe { printf(b"\n\0" as *const u8); }
+    println!("{}, {}", deque.head, deque.tail); // `32, 31'
+
     deque.push_back(6);                         // CHERI: length violation!
-    for x in &deque {
-        print_sp(*x);                           // `5 0'
-    }
-    unsafe { printf(b"\n\0" as *const u8); }
-    print_sp(deque.head as i64);                // `1'
-    print_sp(deque.tail as i64);                // `31'
-    unsafe { printf(b"\n\0" as *const u8); }
+    println!("{:?}", deque);                    // `[5, 0]' <- not [5, 6]
+    println!("{}, {}", deque.head, deque.tail); // `1, 31'
 }
