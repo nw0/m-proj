40b337787f54e61b2632b6ece3ff0d3fbfff0af8
Nicholas Sim <nsim@posteo.net>
2019-05-10 16:49:31 +0100

    dis: eval: lst slice-repeat: caption, add some content

--- dissertation/listings/slice-repeat.rs
+++ dissertation/listings/slice-repeat.rs
@@ -1,15 +1,22 @@
 impl<T> [T] {
     pub fn repeat(&self, n: usize) -> Vec<T> where T: Copy {
-        // omitted: trivial n = 0 case
-        let mut buf = Vec::with_capacity(self.len() * n);
+        if n == 0 { return Vec::new(); }
 
+        let mut buf = Vec::with_capacity(self.len() * n);
         buf.extend(self);
         {
             let mut m = n >> 1;
             while m > 0 {
                 unsafe {
-                    // memcpy existing elements to double the length
+                    ptr::copy_nonoverlapping(
+                        buf.as_ptr(),
+                        (buf.as_mut_ptr() as *mut T).add(buf.len()),
+                        buf.len(),
+                    );
+                    let buf_len = buf.len();
+                    buf.set_len(buf_len * 2);
                 }
+                m >>= 1;
             }
         }
         // omitted: copy into the remainder of the vector
