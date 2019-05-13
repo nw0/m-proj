538bb3693e932bf119fdb0ccc3ab7d964e134416
Nicholas Sim <nsim@posteo.net>
2019-05-13 12:07:03 +0100

    dis: eval: micro-cap introduce prntf!

--- dissertation/listings/micro-capacity.rs
+++ dissertation/listings/micro-capacity.rs
@@ -3,13 +3,13 @@ fn main() {
 
     let mut deque = VecDeque::with_capacity(31);
     deque.push_front(5);
-    println!("{:?}", deque);                    // `[5]'
-    println!("{}, {}", deque.head, deque.tail); // `0, 31'
+    for x in &deque { prntf!("%d ", *x); } prntf!("\n");        // `5'
+    prntf!("%d, %d", deque.head as u64, deque.tail as u64);     // `0, 31'
 
     deque.reserve(30);                          // head should not change
-    println!("{}, {}", deque.head, deque.tail); // `32, 31'
+    prntf!("%d, %d", deque.head as u64, deque.tail as u64);     // `32, 31'
 
     deque.push_back(6);                         // CHERI: length violation!
-    println!("{:?}", deque);                    // `[5, 0]' <- not [5, 6]
-    println!("{}, {}", deque.head, deque.tail); // `1, 31'
+    for x in &deque { prntf!("%d ", *x); }      // `5 0' <- not `5 6'!
+    prntf!("\n%d, %d", deque.head as u64, deque.tail as u64);   // `1, 31'
 }
