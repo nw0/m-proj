da9cc532e60da89de0cec7e8fb82cc634659927e
Nicholas Sim <nsim@posteo.net>
2019-05-17 13:05:23 +0100

    listings: boundscheck-128: fmt, 64: use mips

--- dissertation/listings/boundscheck-usize64.s
+++ dissertation/listings/boundscheck-usize64.s
@@ -1,5 +1,17 @@
 bounds_usize64:
-        lui     $1, %hi(%neg(%gp_rel(bounds_usize64)))
-        daddu   $3, $1, $25
-        sltu    $1, $6, $5
-        beqz    $1, .LBB1_2     ; panic
+        daddiu  sp,sp,-64
+        sd      ra,56(sp)
+        sd      s8,48(sp)
+        sd      gp,40(sp)
+        move    s8,sp
+        lui     at,0x1
+        daddu   at,at,t9
+        daddiu  at,at,31600
+        move    t9,a2
+        move    v0,a1
+        sltu    v1,a2,a1
+        sd      at,32(s8)
+        sd      t9,24(s8)
+        sd      v0,16(s8)
+        sd      a0,8(s8)
+        beqz    v1,1051c        ; panic
