c3d769933dbe8ecf3195bd6075590d5d91ed3b7a
Nicholas Sim <nsim@posteo.net>
2019-05-17 13:51:04 +0100

    dis: lst-boundschecks128: annotate

--- dissertation/listings/boundscheck-usize128.s
+++ dissertation/listings/boundscheck-usize128.s
@@ -1,8 +1,8 @@
 bounds_usize128:
-        xor     a5,a2,a0
+        xor     a5,a2,a0        ; xor upper bits of idx, len
         sltu    a6,a2,a0
-        xori    a6,a6,0x1
+        xori    a6,a6,0x1       ; cmp upper bits
         sltu    a7,a3,a1
-        xori    a7,a7,0x1
-        movz    a6,a7,a5
+        xori    a7,a7,0x1       ; cmp lower bits
+        movz    a6,a7,a5        ; use lower bits if upper bits eq
         bnez    a6,10510        ; panic
--- dissertation/listings/boundscheck-usize64.s
+++ dissertation/listings/boundscheck-usize64.s
@@ -1,3 +1,3 @@
 bounds_usize64:
-        sltu    v1,a2,a1
+        sltu    v1,a2,a1        ; idx < len ?
         beqz    v1,1051c        ; panic
